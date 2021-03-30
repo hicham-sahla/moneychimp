ccebc2dce4ec3c28d580ec2bdb5783e3e98340f2
xiaonanpols21 <74655427+xiaonanpols21@users.noreply.github.com>
2021-03-30 16:16:41 +0200

    Timeout functie

--- assets/js/mijn-aap.js
+++ assets/js/mijn-aap.js
@@ -8,21 +8,35 @@ var knopBanaan = document.querySelector(".knop-banaan");
 // Afbeelding die veranderd
 var imgUser = document.querySelector("#user-aap");
 
+// Code van: https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing1
+function terugAap() {
+    imgUser.src = "../../assets/img/mijn-aap-happy.gif";
+}
+
 // Functies van de knoppen
 function gekozenHand(){
     imgUser.src = "../../assets/img/mijn-aap-hand.gif";
+
+    // Code van: https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing1
+    setTimeout(terugAap, 4120);
 }
 
 knopHand.addEventListener("click", gekozenHand);
 
 function gekozenDansen(){
     imgUser.src = "../../assets/img/mijn-aap-dansen.gif";
+
+    // Code van: https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing1
+    setTimeout(terugAap, 4120);
 }
 
 knopMuziek.addEventListener("click", gekozenDansen);
 
 function gekozenBanaan(){
     imgUser.src = "../../assets/img/mijn-aap-eten.gif";
+
+    // Code van: https://www.w3schools.com/js/tryit.asp?filename=tryjs_timing1
+    setTimeout(terugAap, 4120);
 }
 
 knopBanaan.addEventListener("click", gekozenBanaan);
