21260b0ad4c605288dd65d33f453faa4671351a1
xiaonanpols21 <74655427+xiaonanpols21@users.noreply.github.com>
2021-03-30 14:42:33 +0200

    Minecraft

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,9 +1,12 @@
 console.log("Welkom bij de shop");
 
 // Vars voor keuzes selecteren eerste stap
-var alleKeuzes = document.querySelector(".img-clmns");
-var keuzeButtonEl = document.querySelector("#keuze-kleur");
+var alleKeuzesKleur = document.querySelector(".clmns-kleur");
+var alleKeuzesMinecraft = document.querySelector(".clmns-kleur");
+var keuzeButtonKleur = document.querySelector("#keuze-kleur");
+var keuzeButtonMinecraft = document.querySelector("#keuze-minecraft");
 var keuzeMenu = document.querySelector(".menu-kleur");
+var keuzeMenuMinecraft = document.querySelector(".menu-minecraft");
 
 // Knoppen kleur
 var knopBruin = document.querySelector(".knop-bruin");
@@ -13,31 +16,35 @@ var knopGroen = document.querySelector(".knop-groen");
 // Afbeelding die veranderd
 var imgUser = document.querySelector("#user-aap");
 
-var gereedEl = document.querySelector(".gereed-btn");
+var gereedElKleur = document.querySelector(".gereed-btn-kleur");
+var gereedElMinecraft = document.querySelector(".gereed-btn-minecraft");
+
+            // Kleur
 
 // Function voor sluiten pop up
 function keuzenSluitenKleur() {
     if (keuzeMenu.style.display === "block") {
         keuzeMenu.style.display = "none";
-        alleKeuzes.style.display = "block";
+        alleKeuzesKleur.style.display = "block";
     } else {
         keuzeMenu.style.display = "block";
     }
 }
 
-gereedEl.addEventListener("click", keuzenSluitenKleur);
+gereedElKleur.addEventListener("click", keuzenSluitenKleur);
 
-// Function voor pop up
+// Function voor pop up kleur
 function keuzenKleur() {
     if (keuzeMenu.style.display === "none") {
         keuzeMenu.style.display = "block";
-        alleKeuzes.style.display = "none"
+        alleKeuzesKleur.style.display = "none"
     } else {
         keuzeMenu.style.display = "none";
+        
     }
 }
 
-keuzeButtonEl.addEventListener("click", keuzenKleur);
+keuzeButtonKleur.addEventListener("click", keuzenKleur);
 keuzeMenu.style.display = "none";
 
 // Functies van de knoppen kleur
@@ -58,3 +65,31 @@ function gekozenGroen(){
 }
 
 knopGroen.addEventListener("click", gekozenGroen);
+
+            // Minecraft
+
+// Function voor pop up minecraft
+function keuzenMinecraft() {
+    if (keuzeMenuMinecraft.style.display === "none") {
+        keuzeMenuMinecraft.style.display = "block";
+        alleKeuzesMinecraft.style.display = "none"
+    } else {
+        keuzeMenuMinecraft.style.display = "none";
+        
+    }
+}
+
+keuzeButtonMinecraft.addEventListener("click", keuzenMinecraft);
+keuzeMenuMinecraft.style.display = "none";
+
+// Function voor sluiten pop up
+function keuzenSluitenMinecraft() {
+    if (keuzeMenuMinecraft.style.display === "block") {
+        keuzeMenuMinecraft.style.display = "none";
+        alleKeuzesMinecraft.style.display = "block";
+    } else {
+        keuzeMenuMinecraft.style.display = "block";
+    }
+}
+
+gereedElMinecraft.addEventListener("click", keuzenSluitenMinecraft);
